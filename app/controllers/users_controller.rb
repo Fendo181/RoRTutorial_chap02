f383454bbec8d99cea7e2aec2f25470c73276f08
endu <futoshi.endo@pepabo.com>
2017-06-22 17:44:47 +0900

    ユーザのshowページにマイクロポストを表示する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,8 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
+      # user側でmicropostの投稿をひっぱてくる
+      @micropost = Micropost.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
